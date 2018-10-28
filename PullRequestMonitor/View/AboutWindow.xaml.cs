f0a90ca015509aa878b06d7b4db53f0b9537dd3f
Nick Form <1095240+nickform@users.noreply.github.com>
2018-10-28 09:21:08 +0000

    Add a placeholder about window with a close button

---
+++ PullRequestMonitor/View/AboutWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace PullRequestMonitor.View
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
